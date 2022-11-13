35e4013503a66f4541d261394c46676ed4a3b01d
Huynh Nhuc <huynhthiminhnhuc@gmail.com>
2022-11-13 18:35:59 +0700

    [ADD] Logic login page

--- lib/src/app/app_config/app_constant.dart
+++ lib/src/app/app_config/app_constant.dart
@@ -6,9 +6,14 @@ abstract class AppConstant {
   static double height = Get.height;
 
   //Api
+  static const token = 'token';
   static const timeOut = 20000;
-  static const baseUrl = 'http://localhost:3069/';
-  static const login = '/user/login';
+  static const baseUrl = 'http://172.16.0.244:8080';
+  static const login = '/account/login';
+
+  //storage
+  static const apiKey = 'meton_api_token';
+
 
   //Infinite list
   static const pageSize = 5;
--- lib/src/common/network/api_interceptor.dart
+++ lib/src/common/network/api_interceptor.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app_sync/src/app/app_config/app_constant.dart';
+import 'package:chat_app_sync/src/app/app_manager.dart';
 import 'package:dio/dio.dart';
 
 class ApiInterceptor extends InterceptorsWrapper{
@@ -5,7 +7,10 @@ class ApiInterceptor extends InterceptorsWrapper{
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // TODO: implement onRequest
+    final appManager = AppManager();
+   if (appManager.isSignIn()){
+    options.headers[AppConstant.token] = appManager.getUserToken();  
+   }
     super.onRequest(options, handler);
   }
   void setHeaderRequest(RequestOptions options) {
